e8216f29f453d5b5a9f587042b07394a8ab81845
matiasbastidas <matias.bastidasm@gmail.com>
2022-08-16 22:00:22 -0400

    Âreadme/cssÂ

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# calculadora-propina
+# Calculadora propina
+
+Calculadora de propinas, con el 10% sugerido en Chile
--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const calcularPropina = () => {
     monto = document.getElementById("monto").value
     personas = document.getElementById("personas").value
-    const propina = 0.10
-    propinaCalculada = (monto * propina) / personas
+
+    const propinaChile = 0.10
+
+    propinaCalculada = (monto * propinaChile) / personas
     total = parseInt(monto) + parseInt(propinaCalculada)
+    
     document.getElementById("montopropina").innerHTML = propinaCalculada
     document.getElementById("total").innerHTML = total
 }
